1589422db90302ee8956899d0b79f94f8860cedf
Max <maxprofkom@gmail.com>
2017-01-14 09:40:17 +0200

    after 7 lesson

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -2,23 +2,12 @@ angular
     .module('phoneCat')
     .component('phoneList', {
         templateUrl: 'phone-list/phone-list.html',
-        controller: function PhoneListController() {
-            this.phones = [
-            {
-                name: 'Nexus S',
-                snippet: 'Fast just got faster with Nexus S.',
-                age: 2
-            }, {
-                name: 'Motorola XOOM™ with Wi-Fi',
-                snippet: 'The Next, Next Generation tablet.',
-                age: 1
-            }, {
-                name: 'MOTOROLA XOOM™',
-                snippet: 'The Next, Next Generation tablet.',
-                age: 3
-                }
-            ];
-
-            this.orderProp = "name";
+        controller: ['$http', function PhoneListController($http) {
+            var self = this;
+            self.orderProp = "name";
+            $http.get('phones/phones.json').then(function(res){
+                self.phones = res.data;
+            })
         }
+        ]
 });
