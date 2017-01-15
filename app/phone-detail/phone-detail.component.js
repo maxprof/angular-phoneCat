bc8633885e542d3e05e1d16830041457a167802c
Max <maxprofkom@gmail.com>
2017-01-15 22:15:49 +0200

    after 12 lesson

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -4,10 +4,16 @@ angular.
     templateUrl: 'phone-detail/phone-detail.html',
     controller: ['$http', '$routeParams',
       function PhoneDetailController($http, $routeParams) {
-        // this.phoneId = $routeParams.phoneId;
         var self = this;
+        self.setImage = function setImage(imageUrl) {
+          self.mainImageUrl = imageUrl;
+        };
+        self.dblClick = function(image){
+          console.log("It's doubleclick on " + image)
+        }
         $http.get('phones/'+ $routeParams.phoneId +'.json').then(function(res){
             self.phone = res.data;
+            self.setImage(self.phone.images[0]);
         })
       }
     ]
